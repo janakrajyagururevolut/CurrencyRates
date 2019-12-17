6df5117a050e3df6d406b9243385643b5dff7369
Janak Rajyaguru <janak.rajyaguru@gmail.com>
2019-12-17 12:41:57 +0000

    Removed unused imports

--- app/src/test/java/com/revolut/android/currencyrates/data/source/remote/CurrencyRatesRemoteDataSourceTest.kt
+++ app/src/test/java/com/revolut/android/currencyrates/data/source/remote/CurrencyRatesRemoteDataSourceTest.kt
@@ -1,16 +1,13 @@
 package com.revolut.android.currencyrates.data.source.remote
 
 import android.content.res.Resources
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.revolut.android.currencyrates.utilities.INITIAL_BASE_CURRENCY
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
-import org.junit.rules.TestRule
-import org.mockito.ArgumentMatchers.anyString
 import org.mockito.Mock
-import org.mockito.Mockito.*
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
 import org.mockito.MockitoAnnotations
 import retrofit2.Retrofit
 
